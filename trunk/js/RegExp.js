6c3f43f5116629c52557df266130818455ba9726
Ildar.Shaimordanov <Ildar.Shaimordanov@bc9aa29c-02eb-11de-af5c-836599625e1f>
2011-02-08 18:21:07 +0000

    experimental library

---
+++ trunk/js/RegExp.js
@@ -0,0 +1,105 @@
+//
+// JavaScript unit
+// Add-on for the RegExp manipulation
+//
+// Copyright (c) 2011 by Ildar Shaimordanov aka Rumata
+//
+
+/*
+
+// RegExp#forEach and RegExp#map are wrappers over the loop like below
+// while ( r = re.exec(str) ) {
+// ...
+// }
+
+// RegExp#match is wrapper over String#match method for the following code
+// var matches = string.match(regexp);
+// var m;
+// if ( matches ) {
+//     m = matches[1];
+// }
+
+
+// Example 1.
+// This exampel will print an each found item
+var regexp = /\d+/g;
+var string = '123 abc 456 def 789 xyz';
+
+regexp.forEach(string, function(r)
+{
+	alert(r);
+});
+
+
+// Example 2.
+// The following example will create an array with all found items
+var regexp = /\d+/g;
+var string = '123 abc 456 def 789 xyz';
+
+var array = regexp.map(string, function(r)
+{
+	return r;
+});
+
+alert(array);
+
+
+// Example 3.
+// This example shows usage of RegExp#match method 
+var re = /\d+/g;
+var s = '123 abc 456 def 789 xyz';
+
+var m2 = re.match(s, 1);
+alert(m2);
+
+*/
+
+if ( ! RegExp.prototype.forEach ) {
+
+RegExp.prototype.forEach = function(string, fun, thisp)
+{
+	if ( typeof fun != "function" ) {
+		throw new TypeError();
+	}
+
+	var r;
+	while ( r = this.exec(string) ) {
+		fun.call(thisp, r, this);
+	}
+};
+
+}
+
+if ( ! RegExp.prototype.map ) {
+
+RegExp.prototype.map = function(string, fun, thisp)
+{
+	if ( typeof fun != "function" ) {
+		throw new TypeError();
+	}
+
+	var result = [];
+
+	var r;
+	while ( r = this.exec(string) ) {
+		result.push(fun.call(thisp, r, this));
+	}
+
+	return result;
+};
+
+}
+
+if ( ! RegExp.prototype.match ) {
+
+RegExp.prototype.match = function(string, index)
+{
+	var m = string.match(this);
+	if ( m && index !== void 0 ) {
+		return m[index];
+		
+	}
+	return m ;
+};
+
+}
