6e47d5a4a16db71801ef7e98513c11c3d82ac4f7
ildar-shaimordanov <ildar.shaimordanov@gmail.com>
2020-11-22 08:40:42 +0300

    improve converting wsf->bat:
    -- enable embedded chimera prolog
    -- skip embedding a file, if it's commented

--- tools/compile-wsf2bat.js
+++ tools/compile-wsf2bat.js
@@ -50,11 +50,13 @@ var chimeraProlog = [
 ].join('\n');
 
 var scriptFile = readFile()
-.replace(/^<\?xml(\s*)(.*\?>)/, function($0, $1, $2) {
-	return '<?xml :\n:' + ( $1 || ' ' ) + $2 + chimeraProlog;
+.replace(/^<\?xml(\s*)(.*\?>)(\s*)(<!-- :[\s\S]+: -->)?/, function($0, $1, $2, $3, $4) {
+	// enable embedded chimera prolog
+	return '<?xml :\n:' + ( $1 || ' ' ) + $2 + ( $4 ? $3 + $4 : chimeraProlog );
 })
-.replace(/<script\s*(.*?)\s*src="([^"]+)"\s*(.*?)\s*>\s*<\/script>/g, function($0, $1, $2, $3) {
-	return '<!-- ' + $0 + ' -->\n<script' + ( $1 && ' ' + $1 ) + ( $3 && ' ' + $3 ) + '><![CDATA[\n' + readFile($2) + '\n]]></script>';
+.replace(/(<!--\s*)?<script\s*(.*?)\s*src="([^"]+)"\s*(.*?)\s*>\s*<\/script>/g, function($0, $1, $2, $3, $4) {
+	// skip embedding a file, if it's commented
+	return $1 ? $0 : '<!-- ' + $0 + ' -->\n<script' + ( $2 && ' ' + $2 ) + ( $4 && ' ' + $4 ) + '><![CDATA[\n' + readFile($3) + '\n]]></script>';
 });
 
 WScript.Echo(scriptFile);
